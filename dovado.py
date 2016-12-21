38e2d62c23f76ea4a552aaad6f79c11541e70c19
Erik <erik.eriksson@yahoo.com>
2016-12-21 13:08:30 +0100

    better error handling. fix strings

--- dovado.py
+++ dovado.py
@@ -26,14 +26,14 @@ def _get_gw():
     # pylint: disable=no-member
     gws = netifaces.gateways()
     gateway = gws['default'][netifaces.AF_INET][0]
-    _LOGGER.info("Using gateway %s", gateway)
+    _LOGGER.info('Using gateway %s', gateway)
     return gateway
 
 
 def _log(what, msg):
     """Helper method for logging."""
     if msg.strip():
-        _LOGGER.debug("%s %s", what, msg.strip().replace("\n", "\\n"))
+        _LOGGER.debug('%s %s', what, msg.strip().replace('\n', '\\n'))
 
 
 class Connection():
@@ -44,33 +44,33 @@ class Connection():
 
     def _until(self, what):
         """Wait for response."""
-        what = what.encode("utf-8")
+        what = what.encode('utf-8')
         ret = self._telnet.read_until(what, timeout=TIMEOUT.seconds)
-        return ret.decode("ascii")
+        return ret.decode('ascii')
 
     def write(self, what):
         """Write data to connection."""
-        _log("send", what)
-        self._telnet.write(what.encode("utf-8"))
+        _log('send', what)
+        self._telnet.write(what.encode('utf-8'))
 
     def send(self, *cmd):
         """Send command to router."""
-        cmd = " ".join(cmd)
-        cursor = ">> "
-        ret = self._until("\n")
-        _log("skip", ret)
+        cmd = ' '.join(cmd)
+        cursor = '>> '
+        ret = self._until('\n')
+        _log('skip', ret)
         ret = self._until(cursor)
-        self.write(cmd + "\n")
+        self.write(cmd + '\n')
         ret = self._until(chr(ETB))[:-1]
-        _log("recv", ret)
+        _log('recv', ret)
         return ret
 
     def query(self, cmd):
         """Make query and convert response into dict."""
         res = self.send(cmd)
-        res = [item.split("=") for item in res.splitlines()]
-        res = [item[0].split(":") if len(item) == 1 else item for item in res]
-        res = {k.lower().replace("_", " "): v for k, v in res}
+        res = [item.split('=') for item in res.splitlines()]
+        res = [item[0].split(':') if len(item) == 1 else item for item in res]
+        res = {k.lower().replace('_', ' '): v for k, v in res}
         return res
 
     @contextmanager
@@ -94,38 +94,45 @@ class Dovado():
     @contextmanager
     def session(self):
         """Open connection to router."""
-        _LOGGER.info("Connecting to %s@%s:%d",
+        _LOGGER.info('Connecting to %s@%s:%d',
                      self._username, self._hostname, self._port)
+
+        def _expect(condition, reason):
+            if not condition:
+                raise RuntimeError(reason)
+
         try:
             with Connection().connect(self._hostname,
                                       self._port) as conn:
-                conn.send("user", self._username)
-                conn.send("pass", self._password)
+                ret = conn.send('user', self._username)
+                _expect('Hello' in ret, 'User unknown')
+                ret = conn.send('pass', self._password)
+                _expect('Access granted' in ret, 'Could not authenticate')
                 yield conn
-                conn.send("quit")
-        except OSError as error:
-            _LOGGER.error("Could not communicate with %s@%s:%d: %s",
+                conn.send('quit')
+        except (RuntimeError, OSError) as error:
+            _LOGGER.error('Could not communicate with %s@%s:%d: %s',
                           self._username, self._hostname, self._port, error)
             raise
 
     def send_sms(self, number, message):
         """Send SMS through the router."""
         with self.session() as conn:
-            res = conn.send("sms sendtxt %s" % number)
-            if "Start sms input" not in res:
+            res = conn.send('sms sendtxt %s' % number)
+            if 'Start sms input' not in res:
                 return False
-            conn.write("%s\n.\n" % message)
+            conn.write('%s\n.\n' % message)
             return True
 
     def query_state(self):
         """Update state from router."""
         with self.session() as conn:
-            info = conn.query("info")
-            services = conn.query("services")
+            info = conn.query('info')
+            services = conn.query('services')
             info.update(services)
             return info
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from sys import argv
     logging.basicConfig(level=logging.DEBUG)
     if len(argv) < 3:
